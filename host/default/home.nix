1f564424e9155e1455a67fdcd71f19ae456d8a93
LuizFelipeOliver <luiz.rya@gmail.com>
2025-08-19 22:51:14 -0300

    fix: test

--- host/default/home.nix
+++ host/default/home.nix
@@ -14,6 +14,7 @@
      init.defaultBranch = "main";
    };
   };
+  
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
