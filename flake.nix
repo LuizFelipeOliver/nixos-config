91b0fb70003b6bffc5a8ba3c2eb7998c8b5126a8
Luiz <zariman.teen@proton.me>
2025-08-19 22:45:33 -0300

    frist commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+       system = "x86_64-linux";
+       pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./host/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
